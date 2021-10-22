d8d282799ce26778f2c81a97a5aa7275726d0f47
Sudheesh S Babu <sudheeshs.babu@piserve.com>
2021-10-22 16:55:03 +0530

    current network and history of connected networks

--- available_networks.py
+++ available_networks.py
@@ -1,5 +1,9 @@
 # importing the subprocess module
-import subprocess
+import subprocess, time
+
+###
+### Currently connected network details
+###
  
 # using the check_output() for having the network term retrieval
 devices = subprocess.check_output(['netsh','wlan','show','network'])
@@ -10,3 +14,50 @@ devices = devices.replace("\r","")
  
 # displaying the information
 print(devices)
+
+###
+### Getting all the Wifi Devices the system has connected
+###
+
+# getting meta data of the wifi network
+meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+ 
+# decoding meta data from byte to string
+data = meta_data.decode('utf-8', errors ="backslashreplace")
+ 
+# splitting data by line by line
+# string to list
+data = data.split('\n')
+ 
+# creating a list of wifi names
+names = []
+ 
+# traverse the list
+for i in data:
+     
+    # find "All User Profile" in each item
+    # as this item will have the wifi name
+    if "All User Profile" in i :
+         
+        # if found split the item
+        # in order to get only the name
+        i = i.split(":")
+         
+        # item at index 1 will be the wifi name
+        i = i[1]
+         
+        # formatting the name
+        # first and last chracter is use less
+        i = i[1:-1]
+         
+        # appending the wifi name in the list
+        names.append(i)
+ 
+# printing the wifi names
+print("All wifi that system has connected to are ")
+print("-----------------------------------------")
+for name in names:
+    print(name)
+
+
+time.sleep(5)
